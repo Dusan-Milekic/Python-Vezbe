b9bf40f4720aa05b9ea3269dc2981bac9e0db393
Dusan Milekic <dusanmilekic0511@gmail.com>
2024-09-14 10:54:37 +0200

    Gotova verzija 1.0

---
+++ my_functions.py
@@ -0,0 +1,22 @@
+import datetime
+import time
+from colorama import Fore, Back, Style
+
+
+def intro_to_program():
+    print(Back.YELLOW + Fore.BLACK + "\n\n" + ("*" * 40) + "\nWelcome to the App DateTime Now!\n" + ("*" * 40) + "\n")
+    print(Style.RESET_ALL)
+
+
+def show_time_now(message_number):
+    my_time = datetime.datetime.now().strftime("%H:%M:%S")
+    if message_number % 2 == 0:
+        print(Fore.LIGHTBLUE_EX + my_time + Fore.RESET)
+    else:
+        print(Fore.MAGENTA + my_time + Fore.RESET)
+
+
+def show_refresh_time():
+    while 1 == 1:
+        show_time_now(0)
+        time.sleep(1)
